0c1d7afc3aeb044ac31cf0c5ef90be70ac74e986
Bishop <christianiradukunda6@gmail.com>
2023-10-08 21:37:17 +0200

    added the user DTO and user Model

---
+++ JwtWebApi/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApi.Models
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public byte[] PasswordHarsh { get; set; }
+        public byte[] PasswordSalt { get; set; }
+    }
+}
